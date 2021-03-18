2b33fb1171f9527b0d3d979d6789ec504219ecb5
Darren Liang <liangdarren2@gmail.com>
2021-03-18 19:34:17 -0400

    render

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
-import React from 'react';
+import React, { useState } from "react";
 import './App.css';
 import Table from './components/Table';
 
-function App() {
+const App = () => {
+  const [rows, setRow] = useState(0);
+  const [cols, setCol] = useState(0);
+  const [color, setColor] = useState("red");
+
+  const addRow = () => {
+    if (rows === 0) {
+      setRow(1);
+      setCol(1);
+    } 
+    else {
+      setRow(rows => rows + 1);
+    }
+    console.log(rows)
+  };
+  const addCol = () => {
+    if (cols === 0) {
+      setRow(1);
+      setCol(1);
+    } 
+    else {
+      setCol(cols => cols + 1);
+    }
+  };
+
+  const removeRow = () => {
+
+  };
+
+  const removeCol = () => {
+
+  };
+
+  const colorSelected = e => setColor(e.target.value);
+  const handleApplyColor = e => (e.target.style.backgroundColor = color);
+
+  const fill = () => {};
+  const fillUncolored = () => {};
+  const clearAll = () => {};
+  
   return (
-    <div className="App">
-      <Table />
+  <div>
+    <h1>ReactGrid</h1>
+      <button onClick={addRow}>Add Row</button>
+      <button onClick={addCol}>Add Column</button>
+      <button onClick={removeRow}>Remove Row</button>
+      <button onClick={removeCol}>Remove Column</button>
+      <button onClick={fillUncolored}>Fill Uncolored</button>
+      <button onClick={fill}>Fill All</button>
+      <button onClick={clearAll}>Clear</button>
+
+      <select onChange={colorSelected}>
+        <option value="red">red</option>
+        <option value="blue">blue</option>
+        <option value="yellow">yellow</option>
+      </select>
+
+      <Table numRows={rows} numCols={cols} handleApplyColor={handleApplyColor} />
+
     </div>
   );
-}
+};
 
 export default App;
